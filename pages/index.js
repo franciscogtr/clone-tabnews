5a4106ff4a9f145f8874d85072fb8cc51d272b53
Francisco Matos <franciscodesousamatos@outlook.com>
2026-03-17 14:14:30 +0000

    Frase melhorada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Mensagem Especial! </h1>
+    return <h1> Miserável é o coração do ser humano que pulsa mais pelo pecado do que o anseio pela eternidade. </h1>
 }
 
 export default Home;
